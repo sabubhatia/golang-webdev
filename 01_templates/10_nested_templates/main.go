e5491d3fc198546576584f7f86d5cf0eab25e0a5
SSB <sabusingh.bhatia@gmail.com>
2021-07-24 13:40:11 +0800

    Added 10_nested templates

---
+++ 01_templates/10_nested_templates/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+
+	"github.com/sabubhatia/golang-webdev/01_templates/utility/fileutil"
+)
+
+var tpl *template.Template
+var path = "./tmp/"
+
+func init() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Expected at least 2 args but got: %d\n", len(os.Args))
+	}
+	tpl = template.Must(template.ParseGlob(os.Args[1]))
+}
+
+func main() {
+	f := fileutil.OutF(path, "Index.doc")
+	defer fileutil.CloseF(f)
+	err := tpl.ExecuteTemplate(f, "Index", nil)
+	if err != nil {
+		log.Fatal("Unable to execute template: ", err)
+	}
+
+	err = tpl.ExecuteTemplate(f, "Odd", nil)
+	if err != nil {
+		log.Fatal("Unable to execute template: ", err)
+	}
+
+	err = tpl.ExecuteTemplate(f, "Even", nil)
+	if err != nil {
+		log.Fatal("Unable to execute template: ", err)
+	}
+
+	f = fileutil.OutF(path, "Genius")
+	defer fileutil.CloseF(f)
+	s := struct {
+		Footer string
+		Header string
+		Body []string
+	} {
+		Footer: "This is a footer",
+		Header: "This is a header",
+		Body: []string {
+			"Laos",
+			"Thailand",
+			"India",
+			"Phillipines",
+		},
+	}
+	err = tpl.ExecuteTemplate(f, "Genius", s)
+	if err != nil {
+		log.Fatal("Unable to execute template: ", err)
+	}
+}
